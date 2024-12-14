cdad84b22e6b41344b6cc6a88be284dfbf91c680
Khushwant Singh <150510696+Khushwant-Singh1@users.noreply.github.com>
2024-12-14 02:45:16 +0000

    api

--- app.py
+++ app.py
@@ -31,16 +31,13 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = OpenAIEmbeddings(openai_api_key=st.secrets["openai"]["api_key"])
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
 
 def get_conversation_chain(vectorstore):
-    llm = ChatOpenAI()
-    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
+    llm = ChatOpenAI(openai_api_key=st.secrets["openai"]["api_key"])
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
